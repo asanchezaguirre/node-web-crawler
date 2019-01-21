b34572917387c937dd10338a01635a4a3f5aabc4
Adriana <s.aguirre.adriana@gmail.com>
2019-01-21 13:15:48 -0600

    Sprint 2, 3, 4

--- download.js
+++ download.js
@@ -1,1 +1,16 @@
-download.js
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const uuidv5 = require('uuid/v5');
+
+const downloadpage = async (URL) => {
+	const folder = await uuidv5(URL, uuidv5.URL)
+	fs.mkdirSync(`./${folder}`, { recursive: true }, (err) =>{
+	if (err) throw err;
+});
+	fs.writeFile(`./${folder}/file.html`, "hola", (err) =>{
+	if (err) throw err;
+	console.log('The file has been saved');
+});
+}
+downloadpage("https://www.google.com/");
