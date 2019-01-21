d93df722df016d0c2ee7005d249740050154e132
Adriana <s.aguirre.adriana@gmail.com>
2019-01-21 17:40:29 -0600

    Proyecto fin

--- download.js
+++ download.js
@@ -4,13 +4,42 @@ const path = require('path')
 const uuidv5 = require('uuid/v5');
 
 const downloadpage = async (URL) => {
+
+	console.log('downloading ' + URL)
+
 	const folder = await uuidv5(URL, uuidv5.URL)
 	fs.mkdirSync(`./${folder}`, { recursive: true }, (err) =>{
 	if (err) throw err;
 });
-	fs.writeFile(`./${folder}/file.html`, "hola", (err) =>{
+
+const fetchPage = (linkUrl, callback) => {
+	let content = "";
+	const req = http.request(linkUrl, function (res){
+		res.setEncoding("utf8");
+		res.on("data", chunk => content+=chunk);
+
+		res.on("end", function(){
+			callback(folder, content, URL);
+		});
+	});
+	req.on('error', e=> console.error(e))
+	req.end();
+	}
+
+	const write = (carpet, data, urlData) =>{
+	fs.writeFile(`./${carpet}/file.html`, data, (err) =>{
 	if (err) throw err;
-	console.log('The file has been saved');
-});
+	//console.log('The file has been saved');
+	});
+	fs.writeFile(`./${carpet}/url.txt`, urlData, (err) =>{
+	if (err) throw err;
+	});
+	
+	console.log('downloading is done in folder ' + carpet)
 }
-downloadpage("https://www.google.com/");
+
+	fetchPage(URL, write)
+}
+
+downloadpage(process.argv[2]);
+
